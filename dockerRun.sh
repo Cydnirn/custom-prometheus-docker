27bc47f332486729710d0f7232ce1fefbf3c2df9
Cydnirn <seno@siotics.org>
2023-12-17 21:55:38 +0700

    feat: Add additional docker run command for linux

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name prometheus -d -p 9091:9091 prometheus-9091 --web.listen-address=:9091 --config.file=/etc/prometheus/prometheus.yml
+docker run --name prometheus --add-host=host.docker.internal:host-gateway -d -p 9091:9091 prometheus-9091 --web.listen-address=:9091 --config.file=/etc/prometheus/prometheus.yml
